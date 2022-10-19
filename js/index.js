4f87f9c7cab0a017da894f5da6d258ef32c335a3
amandab <amanda.pp@gmail.com>
2022-10-19 19:03:22 -0300

    pronto

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
 // Iteration 1: Names and Input
-//
+
+console.log("I'm ready");
+
+let hacker1 = "Mark";
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Steve";
+
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`);
+} else if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+}
 
 // Iteration 3: Loops
 
-let name = "Amanda"
+let nomeFinal  = "";
 
-console.log(name)
+
+for (let i = 0; i < hacker1.length; i++) {
+    
+    nomeFinal += `${hacker1[i].toUpperCase()} `
+}
+
+console.log(nomeFinal);
+
